01d1773e33c6930875917b6ef974978295c31d6e
D. Prameswara <meshwara@gmail.com>
2022-12-08 12:25:20 +0700

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="flex-container">
+        <div class="content">
+            <button @click="downloadFile('success')">Download success</button>
+            <button @click="downloadFile('fail')">Download fail</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import axios from "axios"
+const URL_API = import.meta.env.VITE_URL_API;
+
+const downloadFile=async (path)=>{
+    try {
+        var res = await axios.get(`${URL_API}/file/${path}`)
+        console.log(res)
+        
+    } catch (error) {
+        console.log(error.response.data)
+    }
+}
+</script>
+
+<style>
+.flex-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    align-items: center;
+}
+.content {
+    display: flex;
+    flex-direction: column;    
+}
+.content button {
+    width: 200px;
+    padding: 10px;
+    margin: 10px;
+}
+</style>
