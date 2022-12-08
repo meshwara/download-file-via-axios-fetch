7c2231d0c27ddd1f51c3130e28315b0c457265e5
D. Prameswara <meshwara@gmail.com>
2022-12-08 12:52:44 +0700

    Add download

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,85 @@
 <template>
     <div class="flex-container">
-        <div class="content">
-            <button @click="downloadFile('success')">Download success</button>
-            <button @click="downloadFile('fail')">Download fail</button>
+        <div class="row">
+            <div class="content">
+                <h3>Menggunakan axios</h3>
+                <button @click="downloadFileAxios('success')">Download success</button>
+                <button @click="downloadFileAxios('fail')">Download fail</button>
+            </div>
+
+            <div class="content">
+                <h3>Menggunakan fetch</h3>
+                <button @click="downloadFileFetch('success')">Download success</button>
+                <button @click="downloadFileFetch('fail')">Download fail</button>
+            </div>
+        </div>
+        <div class="error">
+            {{ lastError }}
         </div>
     </div>
+
 </template>
 
 <script setup>
 import axios from "axios"
+import { ref } from "vue";
 const URL_API = import.meta.env.VITE_URL_API;
+const lastError = ref("")
 
-const downloadFile=async (path)=>{
+const downloadFileAxios = async (path) => {
     try {
-        var res = await axios.get(`${URL_API}/file/${path}`)
-        console.log(res)
-        
+        lastError.value = ""
+        var res = await axios({
+            url: `${URL_API}/file/${path}`,
+            method: 'GET',
+            responseType: 'blob', // important
+        }).then((response) => {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'berkas.pdf');
+            document.body.appendChild(link);
+            link.click();
+        }).catch(e => {
+            // di sini tipe error adalah blob, sehingga tidak bisa kita ubah menjadi json
+            // maka, berikan error yang umum
+            lastError.value = "Gagal download file"
+        })
+
     } catch (error) {
-        console.log(error.response.data)
+        console.log(error)
+    }
+}
+
+const downloadFileFetch = async (path) => {
+    try {
+        lastError.value = ""
+        await fetch(`${URL_API}/file/${path}`)
+            .then((response) => {
+                if (response.ok) {
+                    return response.blob()
+                } else {
+                    return Promise.reject(response)
+                }
+            })
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', 'berkas.pdf');
+                document.body.appendChild(link);
+                link.click();
+            })
+            .catch(async (e) => {
+                var data = await e.json()
+                return Promise.reject(data)
+            })
+            .catch(e => {
+                lastError.value = e.message
+            })
+
+    } catch (error) {
+        console.log(error)
     }
 }
 </script>
@@ -30,13 +92,24 @@ const downloadFile=async (path)=>{
     height: 100%;
     align-items: center;
 }
+
 .content {
     display: flex;
-    flex-direction: column;    
+    flex-direction: column;
 }
+
 .content button {
     width: 200px;
     padding: 10px;
     margin: 10px;
+}
+
+.row {
+    display: flex;
+    flex-direction: row;
+}
+
+.error {
+    color: red;
 }
 </style>
