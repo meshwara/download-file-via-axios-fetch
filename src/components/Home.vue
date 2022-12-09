3e599a31c4750b47bc7e35c6e73332d11a244041
D. Prameswara <meshwara@gmail.com>
2022-12-09 12:55:22 +0700

    Ubah responseTYpe agar bisa di convert ke JSON

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,63 +23,54 @@
 <script setup>
 import axios from "axios"
 import { ref } from "vue";
+
 const URL_API = import.meta.env.VITE_URL_API;
 const lastError = ref("")
 
 const downloadFileAxios = async (path) => {
     try {
         lastError.value = ""
-        var res = await axios({
+        var dt = await axios({
             url: `${URL_API}/file/${path}`,
             method: 'GET',
-            responseType: 'blob', // important
-        }).then((response) => {
-            const url = window.URL.createObjectURL(new Blob([response.data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', 'berkas.pdf');
-            document.body.appendChild(link);
-            link.click();
-        }).catch(e => {
-            // di sini tipe error adalah blob, sehingga tidak bisa kita ubah menjadi json
-            // maka, berikan error yang umum
-            lastError.value = "Gagal download file"
+            // isi respone type dengan array buffer karene respone bisa file/blob atau json
+            responseType: 'arraybuffer',
+            transformResponse: (data, _, status) => {
+                // ubah data menjadi tipe yang sesuai dengan melihat http status nya
+                return status == 200 ? window.URL.createObjectURL(new Blob([data])) : JSON.parse(String.fromCharCode.apply(null, new Uint8Array(data)))
+            }
         })
 
+        const link = document.createElement('a');
+        link.href = dt.data;
+        link.setAttribute('download', 'berkas.pdf');
+        document.body.appendChild(link);
+        link.click();
     } catch (error) {
-        console.log(error)
+        lastError.value = error.response?.data?.message ?? "Gagal download file"
     }
 }
 
 const downloadFileFetch = async (path) => {
     try {
         lastError.value = ""
-        await fetch(`${URL_API}/file/${path}`)
-            .then((response) => {
-                if (response.ok) {
-                    return response.blob()
-                } else {
-                    return Promise.reject(response)
-                }
-            })
-            .then(blob => {
-                const url = window.URL.createObjectURL(blob);
-                const link = document.createElement('a');
-                link.href = url;
-                link.setAttribute('download', 'berkas.pdf');
-                document.body.appendChild(link);
-                link.click();
-            })
-            .catch(async (e) => {
-                var data = await e.json()
-                return Promise.reject(data)
-            })
-            .catch(e => {
-                lastError.value = e.message
-            })
+        var blob = null
+        var response = await fetch(`${URL_API}/file/${path}`)
 
+        if (response.ok) {
+            blob = await response.blob()
+        } else {
+            throw response
+        }
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'berkas.pdf');
+        document.body.appendChild(link);
+        link.click();
     } catch (error) {
-        console.log(error)
+        var e = await error.json()
+        lastError.value = e.message ?? "Gagal download file"
     }
 }
 </script>
